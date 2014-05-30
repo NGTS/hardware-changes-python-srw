2f6cf1bb87cd4ebbdc7b472381eb35e8613592db
Simon Walker <s.r.walker101@googlemail.com>
2014-05-30 12:22:18 +0100

    moving test code into unittest class
    this class now has proper setup

--- testing/test_datastore.py
+++ testing/test_datastore.py
@@ -3,7 +3,10 @@ import datetime
 import random
 import time
 import os
-from hardware_changes.datastore import get_id, update
+import unittest
+import pytest
+
+from hardware_changes.datastore import get_id, update, DatabaseIntegrityError
 
 connection = MySQLdb.connect(host='sirius.astro.warwick.ac.uk', db='ngts_hwlog', user='sw')
 camera_names = [800 + value for value in xrange(1, 14)]
@@ -37,57 +40,57 @@ def print_status(cursor):
         print "Camera {}, telescope {}, started {}".format(*row)
     print
 
-def main():
-    clean_database()
+class TestDataStore(unittest.TestCase):
+    def setUp(self):
+        clean_database()
 
-    # Insert all of the cameras
-    with connection as cursor:
-        cursor.executemany('''insert into camera (camera_name) values (%s)''',
-                [(c, ) for c in camera_names])
-        cursor.executemany('''insert into telescope (telescope_name) values (%s)''',
-                [(t, ) for t in telescope_names])
+        # Insert all of the cameras
+        with connection as cursor:
+            cursor.executemany('''insert into camera (camera_name) values (%s)''',
+                    [(c, ) for c in camera_names])
+            cursor.executemany('''insert into telescope (telescope_name) values (%s)''',
+                    [(t, ) for t in telescope_names])
 
-    update(connection.cursor(), random.choice(camera_names), random.choice(telescope_names),
-            update_time = lambda: datetime.datetime(2013, 10, 5, 0, 0, 0))
-    with connection as cursor:
-        print_status(cursor)
-
-    update(connection.cursor(), random.choice(camera_names), random.choice(telescope_names),
-            update_time = lambda: datetime.datetime(2013, 10, 5, 0, 0, 0))
-    with connection as cursor:
-        print_status(cursor)
-
-    update(connection.cursor(), random.choice(camera_names), random.choice(telescope_names),
-            update_time = lambda: datetime.datetime(2013, 10, 5, 0, 0, 0))
-    with connection as cursor:
-        print_status(cursor)
-
-    update(connection.cursor(), random.choice(camera_names), random.choice(telescope_names),
-            update_time = lambda: datetime.datetime(2013, 10, 5, 0, 0, 0))
-    with connection as cursor:
-        print_status(cursor)
-
-    print "Running interrupt"
-    try:
+    def test_all_code(self):
         update(connection.cursor(), random.choice(camera_names), random.choice(telescope_names),
-                update_time = lambda: datetime.datetime(2013, 10, 5, 0, 0, 0),
-                interrupt=True)
-    except RuntimeError:
-        pass
-    with connection as cursor:
-        print_status(cursor)
+                update_time = lambda: datetime.datetime(2013, 10, 5, 0, 0, 0))
+        with connection as cursor:
+            print_status(cursor)
 
-    for i in xrange(100):
-        update(connection.cursor(), random.choice(camera_names), random.choice(telescope_names))
+        update(connection.cursor(), random.choice(camera_names), random.choice(telescope_names),
+                update_time = lambda: datetime.datetime(2013, 10, 5, 0, 0, 0))
+        with connection as cursor:
+            print_status(cursor)
 
-    with connection as cursor:
-        print_status(cursor)
+        update(connection.cursor(), random.choice(camera_names), random.choice(telescope_names),
+                update_time = lambda: datetime.datetime(2013, 10, 5, 0, 0, 0))
+        with connection as cursor:
+            print_status(cursor)
 
-    print 'Inserting an invalid camera'
-    bad_camera_id = 10101
-    assert bad_camera_id not in camera_names
-    update(connection.cursor(), bad_camera_id, random.choice(telescope_names))
+        update(connection.cursor(), random.choice(camera_names), random.choice(telescope_names),
+                update_time = lambda: datetime.datetime(2013, 10, 5, 0, 0, 0))
+        with connection as cursor:
+            print_status(cursor)
 
-if __name__ == '__main__':
-    main()
+        print "Running interrupt"
+        try:
+            update(connection.cursor(), random.choice(camera_names), random.choice(telescope_names),
+                    update_time = lambda: datetime.datetime(2013, 10, 5, 0, 0, 0),
+                    interrupt=True)
+        except RuntimeError:
+            pass
+        with connection as cursor:
+            print_status(cursor)
+
+        for i in xrange(100):
+            update(connection.cursor(), random.choice(camera_names), random.choice(telescope_names))
+
+        with connection as cursor:
+            print_status(cursor)
+
+    def test_inserting_bad_camera_check_in_interface(self):
+        bad_camera_id = 10101
+        assert bad_camera_id not in camera_names
+        with pytest.raises(DatabaseIntegrityError):
+            update(connection.cursor(), bad_camera_id, random.choice(telescope_names))
 
