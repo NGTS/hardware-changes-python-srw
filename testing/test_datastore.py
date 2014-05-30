273415c9bb65e79b25d2ffaad9cabf1c565569d9
Simon Walker <s.r.walker101@googlemail.com>
2014-05-30 12:32:55 +0100

    Using helper functions to get random camera/telescope

--- testing/test_datastore.py
+++ testing/test_datastore.py
@@ -52,30 +52,36 @@ class TestDataStore(unittest.TestCase):
             cursor.executemany('''insert into telescope (telescope_name) values (%s)''',
                     [(t, ) for t in self.telescope_names])
 
+    def random_camera(self):
+        return random.choice(self.camera_names)
+
+    def random_telescope(self):
+        return random.choice(self.telescope_names)
+
     def test_all_code(self):
-        update(self.connection.cursor(), random.choice(self.camera_names), random.choice(self.telescope_names),
+        update(self.connection.cursor(), self.random_camera(), self.random_telescope(),
                 update_time = lambda: datetime.datetime(2013, 10, 5, 0, 0, 0))
         with self.connection as cursor:
             print_status(cursor)
 
-        update(self.connection.cursor(), random.choice(self.camera_names), random.choice(self.telescope_names),
+        update(self.connection.cursor(), self.random_camera(), self.random_telescope(),
                 update_time = lambda: datetime.datetime(2013, 10, 5, 0, 0, 0))
         with self.connection as cursor:
             print_status(cursor)
 
-        update(self.connection.cursor(), random.choice(self.camera_names), random.choice(self.telescope_names),
+        update(self.connection.cursor(), self.random_camera(), self.random_telescope(),
                 update_time = lambda: datetime.datetime(2013, 10, 5, 0, 0, 0))
         with self.connection as cursor:
             print_status(cursor)
 
-        update(self.connection.cursor(), random.choice(self.camera_names), random.choice(self.telescope_names),
+        update(self.connection.cursor(), self.random_camera(), self.random_telescope(),
                 update_time = lambda: datetime.datetime(2013, 10, 5, 0, 0, 0))
         with self.connection as cursor:
             print_status(cursor)
 
         print "Running interrupt"
         try:
-            update(self.connection.cursor(), random.choice(self.camera_names), random.choice(self.telescope_names),
+            update(self.connection.cursor(), self.random_camera(), self.random_telescope(),
                     update_time = lambda: datetime.datetime(2013, 10, 5, 0, 0, 0),
                     interrupt=True)
         except RuntimeError:
@@ -84,7 +90,7 @@ class TestDataStore(unittest.TestCase):
             print_status(cursor)
 
         for i in xrange(100):
-            update(self.connection.cursor(), random.choice(self.camera_names), random.choice(self.telescope_names))
+            update(self.connection.cursor(), self.random_camera(), self.random_telescope())
 
         with self.connection as cursor:
             print_status(cursor)
@@ -93,7 +99,7 @@ class TestDataStore(unittest.TestCase):
         bad_camera_id = 10101
         assert bad_camera_id not in self.camera_names
         with pytest.raises(DatabaseIntegrityError):
-            update(self.connection.cursor(), bad_camera_id, random.choice(self.telescope_names))
+            update(self.connection.cursor(), bad_camera_id, self.random_telescope())
 
     def test_inserting_bad_camera_check_in_database(self):
         '''
